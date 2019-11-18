2e36b17292767d124f42bc0d61a2417c181ba540
Berry <berry.hijwegen@student.hu.nl>
2019-11-18 22:20:09 +0100

    Added rotation physics

--- Week 1/Physics Unity/Fysica.cs
+++ Week 1/Physics Unity/Fysica.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Fysica : MonoBehaviour
 {
+    public GameObject Origin;       // Origin object can be defined in Unity, object with script applied will use given object as origin
     public Vector3 Force;
     public Vector3 Acceleration;
-    public Vector3 Velocity;
-    public Vector3 startForce;
-    public int mass;
-    public int stretch;
-    public int gravity;
+    public Vector3 Velocity;        // Set y value for spring movement, change x-asis value for rotation
+    public int Mass;                // Set to 1 for spring movement/rotation
+    public int Stretch;             // Set to 10 for spring movement/rotation
+    public int Gravity;             // Set to 1 for spring movement, 0 for rotation
 
     void FixedUpdate()
     {
-        Force = -stretch * transform.position + -gravity * Velocity;
-        Acceleration = Force / mass;
+        Force = -Stretch * (transform.position - Origin.transform.position) + -Gravity * Velocity;
+        Acceleration = Force / Mass;
         Velocity += Acceleration * Time.deltaTime;
         transform.position += Velocity * Time.deltaTime;
     }
