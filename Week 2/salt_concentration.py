d10e649d54ed1c95eb4431e0426953c602bb950b
Berry <berry.hijwegen@student.hu.nl>
2019-11-21 14:05:04 +0100

    Prettified plot

--- Week 2/salt_concentration.py
+++ Week 2/salt_concentration.py
@@ -1,38 +1,35 @@
 import matplotlib.pyplot as plt
 
-# Set parameters
-in_speed = 6
-salt_concentration_in = 0.1
-out_speed = 5
-total_water = 1000
-total_salt = 0
+def plot_salt_concentration_over_time(in_speed=6, salt_concentration_in=0.1, out_speed=6, total_water=1000, total_salt=0, t_end=1000, line_color="-r"):
+    # Initialize results array
+    times = [0]
+    values = [0]
 
-# Initialize results array
-times = [0]
-values = [0]
+    # Initialize time variable
+    t = 0
 
-# Initialize time variable
-t = 0
-t_end = 1000
+    while t < t_end:
+        # Flow in
+        total_water += in_speed
+        total_salt += salt_concentration_in * in_speed
+        salt_concentration = total_salt / total_water
 
-while t < t_end:
-    # Flow in
-    total_water += in_speed
-    total_salt += salt_concentration_in * in_speed
-    salt_concentration = total_salt / total_water
+        # Flow out
+        total_water -= out_speed
+        total_salt -= salt_concentration * out_speed
+        salt_concentration = total_salt / total_water
+        
+        # Set new time
+        t+=1
 
-    # Flow out
-    total_water -= out_speed
-    total_salt -= salt_concentration * out_speed
-    salt_concentration = total_salt / total_water
-    
-    # Set new time
-    t+=1
+        # Add values to results
+        times.append(t)
+        values.append(salt_concentration)
 
-    # Add values to results
-    times.append(t)
-    values.append(salt_concentration)
+    # Create the plot
+    plt.plot(times,values, line_color, label=f"Out Speed: {out_speed}")
 
-# Create the plot
-plt.plot(times,values)
+plot_salt_concentration_over_time()
+plot_salt_concentration_over_time(out_speed=5, line_color="-y")
+plt.legend()
 plt.show()
